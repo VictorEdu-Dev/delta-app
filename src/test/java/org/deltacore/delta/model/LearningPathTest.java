0035545682b7d6a8da2ea43828024f2ef0686274
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-01 12:22:13 -0300

    Add Tests.

---
+++ src/test/java/org/deltacore/delta/model/LearningPathTest.java
@@ -0,0 +1,58 @@
+package org.deltacore.delta.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LearningPathTest {
+
+    @Test
+    void getTitle() {
+        System.out.println("Hello");
+    }
+
+    @Test
+    void getDescription() {
+        System.out.println("Hello");
+    }
+
+    @Test
+    void getImageUrl() {
+        System.out.println("Hello");
+    }
+
+    @Test
+    void getRecommendedLevel() {
+        System.out.println("Hello");
+    }
+
+    @Test
+    void getTotalScore() {
+        System.out.println("Hello");
+    }
+
+    @Test
+    void setLearningPathId() {
+        System.out.println("Hello");
+    }
+
+    @Test
+    void setTitle() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void setImageUrl() {
+    }
+
+    @Test
+    void setRecommendedLevel() {
+    }
+
+    @Test
+    void setTotalScore() {
+    }
+}
