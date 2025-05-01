b96309ca2ae4f4a638ff2767b3d63c62ba89da44
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-01 12:22:01 -0300

    Main project.

---
+++ src/main/java/org/deltacore/delta/DeltaAppApplication.java
@@ -0,0 +1,13 @@
+package org.deltacore.delta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeltaAppApplication.class, args);
+    }
+
+}
