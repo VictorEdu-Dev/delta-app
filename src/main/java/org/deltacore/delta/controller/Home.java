5685b8cf4ccda710d23d3c07fe7d56e831242eab
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-01 16:39:36 -0300

    Tests on rest controller.

--- src/main/java/org/deltacore/delta/controller/Home.java
+++ src/main/java/org/deltacore/delta/controller/Home.java
@@ -1,24 +1,41 @@
 package org.deltacore.delta.controller;
 
-import jakarta.websocket.server.PathParam;
 import org.deltacore.delta.dto.UserDTO;
+import org.deltacore.delta.service.GeneralUserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController()
 public class Home {
 
+    private final GeneralUserService generalUserService;
 
-    @PostMapping(path = "/home/", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<UserDTO> home(@RequestBody UserDTO user, long id) {
+    @Autowired
+    public Home(GeneralUserService generalUserService) {
+        this.generalUserService = generalUserService;
+    }
 
-        if (id == 123) {
-            return ResponseEntity.ok(UserDTO.builder().username(user.getUsername()).passwordHash(user.getPasswordHash()).build());
+    @GetMapping(path = "/register/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDTO> home(@PathVariable String username) {
+        Optional<UserDTO> user = Optional.ofNullable(generalUserService.getUserDBByUsername(username));
+        return user.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping(path = "/register", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<UserDTO>> home(@RequestBody UserDTO user) {
+        try {
+            generalUserService.saveUserDB(user);
+            return ResponseEntity.ok(generalUserService.getAllUsers());
+        } catch (Exception e) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(null);
         }
-
-        return ResponseEntity
-                .ok(user);
     }
 
 }
