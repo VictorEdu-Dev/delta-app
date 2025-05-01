993a8bd8a593d07c522402c4deabaf0999c3670a
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-01 16:37:14 -0300

    Add initial data model.

--- src/main/java/org/deltacore/delta/model/VideoLesson.java
+++ src/main/java/org/deltacore/delta/model/VideoLesson.java
@@ -1,4 +1,26 @@
 package org.deltacore.delta.model;
 
-public class VideoLesson {
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Builder
+@EqualsAndHashCode(callSuper = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class VideoLesson extends GeneralData {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String title;
+    private String description;
+    private String videoUrl;
+    private Integer videoDuration;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity_id")
+    private Activity activity;
 }
