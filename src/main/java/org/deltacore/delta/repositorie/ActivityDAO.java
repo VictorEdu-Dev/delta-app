306d68ec680e2d4b3cffc5185eec9456225130af
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-05 22:46:28 -0300

    Add ActivityDAO interface for activity data access.

---
+++ src/main/java/org/deltacore/delta/repositorie/ActivityDAO.java
@@ -0,0 +1,36 @@
+package org.deltacore.delta.repositorie;
+
+import org.deltacore.delta.model.Activity;
+import org.deltacore.delta.model.ActivityType;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface ActivityDAO extends CrudRepository<Activity, UUID> {
+    @Query(value = "SELECT * FROM activity WHERE LOWER(TRIM(title)) LIKE LOWER(CONCAT('%', '?1', '%')) LIMIT ?2)", nativeQuery = true)
+    Iterable<Activity> findActivitiesByTitle(String title, Integer limit);
+
+    @Query(value = "SELECT * FROM activity WHERE LOWER(TRIM(title)) LIKE LOWER(CONCAT('%', '?1', '%')) LIMIT 10)", nativeQuery = true)
+    Iterable<Activity> findActivitiesByTitle(String title);
+
+
+    @Query(value = "SELECT * FROM activity WHERE title LIKE %?1% AND activityType = ?2 LIMIT ?3", nativeQuery = true)
+    Iterable<Activity> findActivitiesByTitleAndType(String title, ActivityType type, Integer limit);
+
+    @Query(value = "SELECT * FROM activity WHERE title LIKE %?1% AND activityType = ?2 LIMIT 10", nativeQuery = true)
+    Iterable<Activity> findActivitiesByTitleAndType(String title, ActivityType type);
+
+
+    @Query(value = "SELECT * FROM activity WHERE activityType = ?1 LIMIT ?2", nativeQuery = true)
+    Iterable<Activity> findActivitiesByActivityType(ActivityType type, Integer limit);
+
+    @Query(value = "SELECT * FROM activity WHERE activityType = ?1", nativeQuery = true)
+    Iterable<Activity> findActivitiesByActivityType(ActivityType type);
+
+    @Query(value = "SELECT * FROM activity LIMIT 10", nativeQuery = true)
+    Iterable<Activity> findAllActivities();
+
+    @Query(value = "SELECT * FROM activity LIMIT ?1", nativeQuery = true)
+    Iterable<Activity> findAllActivities(Integer limit);
+}
