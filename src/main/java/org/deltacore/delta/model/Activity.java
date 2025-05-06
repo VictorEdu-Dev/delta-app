4c1e39d4e0ba1bb4c7b52ad0a21a989479cde596
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-05 22:46:45 -0300

    Refactor Activity class to use BigDecimal for maxScore and add subject relationship.

--- src/main/java/org/deltacore/delta/model/Activity.java
+++ src/main/java/org/deltacore/delta/model/Activity.java
@@ -3,6 +3,7 @@ package org.deltacore.delta.model;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
@@ -21,7 +22,10 @@ public class Activity extends GeneralData {
     private ActivityType activityType;
     private String imageUrl;
     private Integer recommendedLevel;
-    private Float maxScore;
+    @Column(precision = 19, scale = 4)
+    private BigDecimal maxScore;
     @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
     private List<VideoLesson> videoUrl;
+    @OneToOne(fetch = FetchType.LAZY)
+    private Subject subject;
 }
