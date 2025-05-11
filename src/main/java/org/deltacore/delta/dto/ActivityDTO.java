58831098a776231b0ffeb96213acb6b83c2aa35d
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-10 22:10:28 -0300

    feat: enhance ActivityDTO with validation constraints for fields

--- src/main/java/org/deltacore/delta/dto/ActivityDTO.java
+++ src/main/java/org/deltacore/delta/dto/ActivityDTO.java
@@ -1,22 +1,36 @@
 package org.deltacore.delta.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import org.deltacore.delta.model.ActivityType;
-import org.deltacore.delta.model.Subject;
-import org.deltacore.delta.model.VideoLesson;
-import org.springframework.lang.NonNull;
+import org.hibernate.validator.constraints.URL;
 
 import java.math.BigDecimal;
-import java.util.List;
 import java.util.UUID;
 
 @Builder
 public record ActivityDTO(
         UUID id,
-        @NonNull String title,
+        @NotBlank(message = "Title cannot be blank.")
+        String title,
+
+        @NotBlank(message = "Description cannot be blank.")
         String description,
-        @NonNull ActivityType activityType,
+
+        @NotNull(message = "Activity type cannot be null.")
+        ActivityType activityType,
+
+        @NotBlank(message = "Image URL cannot be blank.")
+        @URL(message = "Image URL must be a valid URL.")
         String imageUrl,
+
+        @Min(value = 1, message = "Recommended level must be at least 1.")
         Integer recommendedLevel,
+
+        @NotNull(message = "Max score cannot be null.")
+        @DecimalMin(value = "0.0", inclusive = false, message = "Max score must be greater than 0.")
         BigDecimal maxScore) {
 }
