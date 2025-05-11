6c86caf0dace7b9fa15be7815073a12a12db0e87
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-11 12:52:17 -0300

    feat: add APIRoutes class for centralized API endpoint management

---
+++ src/main/java/org/deltacore/delta/controller/APIRoutes.java
@@ -0,0 +1,9 @@
+package org.deltacore.delta.controller;
+
+public class APIRoutes {
+    public static final String BASE_API = "/activities";
+    public static final String LIST_ACTIVITIES = BASE_API + "/list-activities";
+
+    private APIRoutes() {
+    }
+}
