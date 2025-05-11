c225bd769bf248b25f74a3f22c641df2b487f74b
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-11 02:49:48 -0300

    feat: add endpoint to list activities and improve saveActivity response

--- src/main/java/org/deltacore/delta/controller/ActivitiesResource.java
+++ src/main/java/org/deltacore/delta/controller/ActivitiesResource.java
@@ -8,13 +8,12 @@ import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.stream.Collectors;
 
-@RestController("/activities")
+@RestController
+@RequestMapping("/activities")
 public class ActivitiesResource {
 
     private final ActivitiesSectionService activitiesService;
@@ -24,17 +23,24 @@ public class ActivitiesResource {
         this.activitiesService = activitiesService;
     }
 
+    @GetMapping(value = "/list-activities", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> getActivities(@RequestParam(value = "search", required = false)  String search) {
+        return ResponseEntity.ok(activitiesService.getLimitedActivities(search));
+    }
+
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> saveActivity(@RequestBody @Valid ActivityDTO activity, BindingResult result) {
-        activitiesService.saveActivity(activity);
+        ActivityDTO dtoActicity = activitiesService.saveActivity(activity);
+
         if (result.hasErrors()) {
             String errorMessage = result.getAllErrors().stream()
                     .map(DefaultMessageSourceResolvable::getDefaultMessage)
                     .collect(Collectors.joining(", "));
             return ResponseEntity.badRequest().body(errorMessage);
         }
-        return ResponseEntity.ok(activity);
+
+        return ResponseEntity.ok(dtoActicity);
     }
 
 
