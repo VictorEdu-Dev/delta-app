b30e97d4809ed33aa49ebcdbff30efe63e806a38
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-10 23:57:40 -0300

    feat: update VideoLesson entity to use sequence generation for ID and enhance field constraints

--- src/main/java/org/deltacore/delta/model/VideoLesson.java
+++ src/main/java/org/deltacore/delta/model/VideoLesson.java
@@ -14,12 +14,25 @@ import java.util.UUID;
 @Entity
 public class VideoLesson extends GeneralData {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "video_lesson_seq")
+    @SequenceGenerator(
+            name = "video_lesson_seq",
+            sequenceName = "video_lesson_sequence"
+    )
+    private Long id;
+
+    @Column(nullable = false, length = 150)
     private String title;
+
+    @Column(length = 500)
     private String description;
+
+    @Column(nullable = false, length = 200)
     private String videoUrl;
+
+    @Column()
     private Integer videoDuration;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "activity_id")
     private Activity activity;
