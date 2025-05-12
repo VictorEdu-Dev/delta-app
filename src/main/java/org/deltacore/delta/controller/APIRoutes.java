f3c276a12430a5a01e2ef5b43b2a167fa986be51
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-11 22:36:38 -0300

    fix: update LIST_ACTIVITIES route to remove base API prefix

--- src/main/java/org/deltacore/delta/controller/APIRoutes.java
+++ src/main/java/org/deltacore/delta/controller/APIRoutes.java
@@ -2,7 +2,7 @@ package org.deltacore.delta.controller;
 
 public class APIRoutes {
     public static final String BASE_API = "/activities";
-    public static final String LIST_ACTIVITIES = BASE_API + "/list-activities";
+    public static final String LIST_ACTIVITIES = "/list-activities";
 
     private APIRoutes() {
     }
