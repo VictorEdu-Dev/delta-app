c1025a5d12fbde582c91d0f01d34465f817b2160
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-11 22:37:39 -0300

    refactor: remove

--- src/main/resources/db.migration/v01__create_table_subject.sql
+++
@@ -1,8 +0,0 @@
-CREATE SEQUENCE subject_sequence;
-
-CREATE TABLE subject (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    code VARCHAR(10) UNIQUE,
-    is_active BOOLEAN NOT NULL
-);
