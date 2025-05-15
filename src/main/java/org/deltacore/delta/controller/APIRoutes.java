efa51a6fd2515b257b61c2b53c510bebb3431e76
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-14 23:47:56 -0300

    feat: add new API route for listing activities with title, status, and deadline

--- src/main/java/org/deltacore/delta/controller/APIRoutes.java
+++ src/main/java/org/deltacore/delta/controller/APIRoutes.java
@@ -3,6 +3,7 @@ package org.deltacore.delta.controller;
 public class APIRoutes {
     public static final String BASE_API = "/activities";
     public static final String LIST_ACTIVITIES = "/list-activities";
+    public static final String LIST_ACTIVITIES_WITH_TITLE_STATUS_DEADLINE_TYPE = "/list-activities-tsdt";
 
     private APIRoutes() {
     }
