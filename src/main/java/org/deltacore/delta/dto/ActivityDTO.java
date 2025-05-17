f4f0e56d3eaba8931d697ac893bb89b9b51b5901
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-17 17:21:04 -0300

    feat: add validation for activity deadline to ensure it is in the future or present

--- src/main/java/org/deltacore/delta/dto/ActivityDTO.java
+++ src/main/java/org/deltacore/delta/dto/ActivityDTO.java
@@ -1,9 +1,6 @@
 package org.deltacore.delta.dto;
 
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.*;
 import lombok.Builder;
 import org.deltacore.delta.model.ActivityStatus;
 import org.deltacore.delta.model.ActivityType;
@@ -37,5 +34,6 @@ public record ActivityDTO(
 
         ActivityStatus status,
 
+        @FutureOrPresent(message = "{activity.deadline.future_or_present}")
         LocalDateTime deadline) {
 }
