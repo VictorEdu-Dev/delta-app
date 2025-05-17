6b188d86aad4565bcdd711d24427518ab5c9d228
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-17 01:29:25 -0300

    feat: add handler for ConflictException in ControllerExceptionHandler

--- src/main/java/org/deltacore/delta/controller/ControllerExceptionHandler.java
+++ src/main/java/org/deltacore/delta/controller/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package org.deltacore.delta.controller;
 
+import org.deltacore.delta.exception.ConflictException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
 
 @RestControllerAdvice
 public class ControllerExceptionHandler {
@@ -18,6 +21,15 @@ public class ControllerExceptionHandler {
         bindingResult.getFieldErrors().forEach(error -> {
             messages.append(error.getDefaultMessage()).append(" \n");
         });
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(messages.toString());
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(messages.toString());
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<?> handleBusiness(ConflictException ex) {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(Map.of("Error:", ex.getMessage()));
     }
 }
