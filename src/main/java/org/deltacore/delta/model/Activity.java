28cc5abcb0c51cb6e292d746622acc34fc04b071
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-24 20:01:35 -0300

    refactor: rename completed column to 'complete' and update completionTimestamp column name

--- src/main/java/org/deltacore/delta/model/Activity.java
+++ src/main/java/org/deltacore/delta/model/Activity.java
@@ -51,10 +51,10 @@ public class Activity extends GeneralData {
     @JoinColumn(name = "subject_id")
     private Subject subject;
 
-    @Column(nullable = false)
-    private boolean completed = false;
+    @Column(name = "complete", nullable = false)
+    private boolean completed;
 
-    @Column
+    @Column(name = "timestamp")
     private LocalDateTime completionTimestamp;
 
 }
