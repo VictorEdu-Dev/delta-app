30e10db825e1ef1e8dc124ce13ee3867afd8991f
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-25 09:58:23 -0300

    refactor: update request mapping for ActivitiesCommand to include 'monitor'

--- src/main/java/org/deltacore/delta/controller/activity/ActivitiesCommand.java
+++ src/main/java/org/deltacore/delta/controller/activity/ActivitiesCommand.java
@@ -11,7 +11,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/activities")
+@RequestMapping("/activities/monitor")
 public class ActivitiesCommand {
     private final ActivitiesSectionService activitiesService;
 
