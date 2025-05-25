0c18b18fea5c7a664d311ba2542a03338475cec0
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-25 10:28:57 -0300

    feat: enhance security configuration with separate filter chains for login and monitoring

--- src/main/java/org/deltacore/delta/config/SecurityDeltaConfig.java
+++ src/main/java/org/deltacore/delta/config/SecurityDeltaConfig.java
@@ -11,7 +11,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -20,14 +20,28 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityDeltaConfig {
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain monitorSecurityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/activities/monitor").hasAuthority("ADMIN")
-                        .anyRequest().authenticated()
+                        .requestMatchers("/activities/monitor/**").hasAuthority("MONITOR")
+                        .anyRequest()
+                        .authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
-                .formLogin(Customizer.withDefaults())
+                .authenticationManager(authenticationManager());
+
+        return http.build();
+    }
+
+    @Bean
+    public SecurityFilterChain loginSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/login/**").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
                 .authenticationManager(authenticationManager());
 
         return http.build();
@@ -64,6 +78,6 @@ public class SecurityDeltaConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new BCryptPasswordEncoder();
     }
 }
