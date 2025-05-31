e1d12889c14316f320f3738b82a8bfef6aa450a5
Jg-365 <jguilherme19@alu.ufc.br>
2025-05-31 19:05:42 -0300

    feat: Add ActivityHistory in the model package

--- src/main/java/org/deltacore/delta/model/ActivityHistory.java
+++ src/main/java/org/deltacore/delta/model/ActivityHistory.java
@@ -1,4 +1,28 @@
 package org.deltacore.delta.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class ActivityHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity_id", nullable = false)
+    private Activity activity;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime changeTimestamp;
 }
