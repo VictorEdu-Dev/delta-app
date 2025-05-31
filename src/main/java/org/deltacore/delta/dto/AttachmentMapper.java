220fa698428fc671b7ded0a456a2ec85244d8448
Jg-365 <jguilherme19@alu.ufc.br>
2025-05-31 19:04:20 -0300

    Add AttachmentMapper interface

--- src/main/java/org/deltacore/delta/dto/AttachmentMapper.java
+++ src/main/java/org/deltacore/delta/dto/AttachmentMapper.java
@@ -1,4 +1,13 @@
 package org.deltacore.delta.dto;
 
-public record AttachmentMapper() {
+import org.deltacore.delta.model.Attachment;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface AttachmentMapper {
+    AttachmentDTO toDTO(Attachment attachment);
+    Attachment toEntity(AttachmentDTO attachmentDTO);
+    List<AttachmentDTO> toDTOList(List<Attachment> attachments);
 }
