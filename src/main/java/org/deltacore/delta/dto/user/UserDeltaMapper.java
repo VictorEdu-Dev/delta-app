66d18c0969ea7e1e2f67853d3dcb9f539abf3694
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-31 18:14:06 -0300

    feat: rename UserMapper to UserDeltaMapper and add updateEntityFromDTO method

--- src/main/java/org/deltacore/delta/dto/user/UserDeltaMapper.java
+++ src/main/java/org/deltacore/delta/dto/user/UserDeltaMapper.java
@@ -2,11 +2,14 @@ package org.deltacore.delta.dto.user;
 
 import org.deltacore.delta.model.user.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper {
+public interface UserDeltaMapper {
 
     User toEntity(UserDTO userDTO);
 
     UserDTO toDTO(User user);
+
+    void updateEntityFromDTO(UserDTO userDTO, @MappingTarget User user);
 }
