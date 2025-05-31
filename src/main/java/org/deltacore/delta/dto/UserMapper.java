13476b867a352955c525ef9b7e96ad4bdd0d4296
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-31 03:33:03 -0300

    feat: add UserMapper interface for mapping between User and UserDTO

---
+++ src/main/java/org/deltacore/delta/dto/UserMapper.java
@@ -0,0 +1,12 @@
+package org.deltacore.delta.dto;
+
+import org.deltacore.delta.model.user.User;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    User toEntity(UserDTO userDTO);
+
+    UserDTO toDTO(User user);
+}
