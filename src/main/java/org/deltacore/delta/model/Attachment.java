820de11c2046f5b0ccb07c05532967b7cba51288
Jg-365 <jguilherme19@alu.ufc.br>
2025-05-31 19:07:00 -0300

    feat: Add Attachment to the model package

--- src/main/java/org/deltacore/delta/model/Attachment.java
+++ src/main/java/org/deltacore/delta/model/Attachment.java
@@ -1,4 +1,26 @@
 package org.deltacore.delta.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Attachment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String fileName;
+
+    @Column(nullable = false)
+    private String fileUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "activity_id", nullable = false)
+    private Activity activity;
 }
