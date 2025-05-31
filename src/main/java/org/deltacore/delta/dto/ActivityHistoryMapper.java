f16d1fb8a682092b284dbe1b3746b4ea52168b22
Jg-365 <jguilherme19@alu.ufc.br>
2025-05-31 19:03:09 -0300

    Add activityHistoryMapper

--- src/main/java/org/deltacore/delta/dto/ActivityHistoryMapper.java
+++ src/main/java/org/deltacore/delta/dto/ActivityHistoryMapper.java
@@ -1,4 +1,13 @@
 package org.deltacore.delta.dto;
 
+import org.deltacore.delta.model.ActivityHistory;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
 public interface ActivityHistoryMapper {
+    ActivityHistoryDTO toDTO(ActivityHistory activityHistory);
+    ActivityHistory toEntity(ActivityHistoryDTO activityHistoryDTO);
+    List<ActivityHistoryDTO> toDTOList(List<ActivityHistory> activityHistories);
 }
