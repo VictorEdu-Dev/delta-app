8a48efec14f6e01b9cfcb1b6f7e68d82eb53a465
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-31 16:18:24 -0300

    feat: update Profile entity to include phone number, total score, and level fields

--- src/main/java/org/deltacore/delta/model/user/Profile.java
+++ src/main/java/org/deltacore/delta/model/user/Profile.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.deltacore.delta.model.Feedback;
 import org.deltacore.delta.model.GeneralData;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
@@ -16,25 +17,32 @@ import java.util.UUID;
 @Entity
 public class Profile extends GeneralData {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
-    @Embedded
-    private FullName fullName;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100)
+    private String name;
+
+    @Lob
+    @Column(name = "profile_image", columnDefinition = "BYTEA")
     private byte[] profileImage;
-    private Float totalScore;
+
+    @Column(length = 15, unique = true)
+    private String phoneNumber;
+
+    @Column(scale = 4, precision = 19)
+    private BigDecimal totalScore;
+
+    @Column(nullable = false)
+    private Integer level;
+
+    @Column(length = 150)
     private String bio;
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
+    @OneToMany(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY, mappedBy = "profile")
     private List<Feedback> feedbacks;
-
-    @Data
-    @Embeddable
-    private static class FullName {
-        String firstName;
-        String lastName;
-    }
 }
