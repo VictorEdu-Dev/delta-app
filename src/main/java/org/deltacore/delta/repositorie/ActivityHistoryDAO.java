a7b0dff82f5ad050bd9533694e796d1fbe391180
Jg-365 <jguilherme19@alu.ufc.br>
2025-05-31 19:08:04 -0300

    feat: add activityHistoryDAO

--- src/main/java/org/deltacore/delta/repositorie/ActivityHistoryDAO.java
+++ src/main/java/org/deltacore/delta/repositorie/ActivityHistoryDAO.java
@@ -1,4 +1,12 @@
 package org.deltacore.delta.repositorie;
 
-public interface ActivityHistoryDAO {
+import org.deltacore.delta.model.ActivityHistory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ActivityHistoryDAO extends JpaRepository<ActivityHistory, Long> {
+    List<ActivityHistory> findByActivityId(Long activityId);
 }
