864cd21c68ba9e0595b21063bdebeccc37394d88
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-31 16:47:21 -0300

    feat: update UserDAO to use Long as ID type and return Optional for findByUsername

--- src/main/java/org/deltacore/delta/repositorie/UserDAO.java
+++ src/main/java/org/deltacore/delta/repositorie/UserDAO.java
@@ -3,8 +3,9 @@ package org.deltacore.delta.repositorie;
 import org.deltacore.delta.model.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.UUID;
+import java.util.Optional;
 
-public interface UserDAO extends JpaRepository<User, UUID> {
-    User findByUsername(String username);
+
+public interface UserDAO extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
