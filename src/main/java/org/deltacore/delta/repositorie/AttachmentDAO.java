093f3b4f788bff876f066abab0ae43a9229ca15d
Jg-365 <jguilherme19@alu.ufc.br>
2025-05-31 19:08:54 -0300

    feat: add AttachmentDAO

--- src/main/java/org/deltacore/delta/repositorie/AttachmentDAO.java
+++ src/main/java/org/deltacore/delta/repositorie/AttachmentDAO.java
@@ -1,4 +1,12 @@
 package org.deltacore.delta.repositorie;
 
-public interface AttachmentDAO {
+import org.deltacore.delta.model.Attachment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AttachmentDAO extends JpaRepository<Attachment, Long> {
+    List<Attachment> findByActivityId(Long activityId);
 }
