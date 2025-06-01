9bb809a5ef2f655491bf1df80eeb2d6011cf1f94
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-31 21:30:17 -0300

    feat: add JwtAuthFilter for JWT authentication in request processing

---
+++ src/main/java/org/deltacore/delta/config/JwtAuthFilter.java
@@ -0,0 +1,51 @@
+package org.deltacore.delta.config;
+
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.deltacore.delta.service.JwtTokenService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtTokenService jwtService;
+    private final UserDetailsService userDetailsService;
+
+    public JwtAuthFilter(JwtTokenService jwtService, UserDetailsService userDetailsService) {
+        this.jwtService = jwtService;
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            try {
+                String username = jwtService.validateTokenAndRetrieveSubject(token);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            } catch (JWTVerificationException ignored) {
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
