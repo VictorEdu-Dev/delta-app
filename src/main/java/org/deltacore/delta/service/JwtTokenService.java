32e8f40c1f857b5ab7adf2fca9e2b735f1a81739
Victor Eduardo <victoreduardodev@gmail.com>
2025-05-31 21:30:22 -0300

    feat: add JwtTokenService for generating and validating JWT tokens

---
+++ src/main/java/org/deltacore/delta/service/JwtTokenService.java
@@ -0,0 +1,42 @@
+package org.deltacore.delta.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.google.api.client.util.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+
+@Service
+public class JwtTokenService {
+
+    private Algorithm getAlgorithm() {
+        String secret = "9PZtBd3RH6Y2M3h8U8nh0R6cWmAQN+itHHeupX7jnhw=";
+        return Algorithm.HMAC256(secret);
+    }
+
+    public String generateToken(String username) throws JWTCreationException {
+        long expirationMillis = 3600000; // 1 hora
+        return JWT.create()
+                .withSubject(username)
+                .withIssuer("DELTA APPLICATION")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + expirationMillis))
+                .sign(getAlgorithm());
+    }
+
+    public String validateTokenAndRetrieveSubject(String token) throws JWTVerificationException {
+        JWTVerifier verifier = JWT.require(getAlgorithm())
+                .withIssuer("DELTA APPLICATION")
+                .build();
+
+        DecodedJWT jwt = verifier.verify(token);
+        return jwt.getSubject();
+    }
+}
+
