022d7c326bd3ce4ad2da181a10e50967747656a5
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-02 20:56:57 -0300

    feat: add StorageGoogleConfig class for GCP Storage configuration

---
+++ src/main/java/org/deltacore/delta/config/StorageGoogleConfig.java
@@ -0,0 +1,18 @@
+package org.deltacore.delta.config;
+
+import com.google.api.client.util.Value;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class StorageGoogleConfig {
+    @Value("${gcp.project.id}")
+    private String projectId;
+
+    @Bean
+    public Storage storage() {
+        return StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+    }
+}
