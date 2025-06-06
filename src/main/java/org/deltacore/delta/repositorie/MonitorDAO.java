11433146a1917df23d137381270a290018305e7a
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-06 17:18:44 -0300

    feat: add MonitorDAO interface for accessing Monitor data by username

---
+++ src/main/java/org/deltacore/delta/repositorie/MonitorDAO.java
@@ -0,0 +1,13 @@
+package org.deltacore.delta.repositorie;
+
+import org.deltacore.delta.model.user.Monitor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface MonitorDAO extends CrudRepository<Monitor,Long> {
+    @Query(value = "SELECT mo.* FROM monitor mo JOIN user_delta us ON mo.user_monitor_id = us.id WHERE us.username = :username", nativeQuery = true)
+    Optional<Monitor> findByUserUsername(@Param("username") String username);
+}
