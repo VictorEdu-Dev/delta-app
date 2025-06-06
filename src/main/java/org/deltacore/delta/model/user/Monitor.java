2270487e6105ee796a16dcc83faced7249b14a6b
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-06 15:32:33 -0300

    feat: refactor Monitor entity to use Long ID and establish one-to-one relationship with Monitoring

--- src/main/java/org/deltacore/delta/model/user/Monitor.java
+++ src/main/java/org/deltacore/delta/model/user/Monitor.java
@@ -3,11 +3,9 @@ package org.deltacore.delta.model.user;
 import jakarta.persistence.*;
 import lombok.*;
 import org.deltacore.delta.model.GeneralData;
-import org.deltacore.delta.model.Subject;
+import org.deltacore.delta.model.monitoring.Monitoring;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
@@ -17,17 +15,20 @@ import java.util.UUID;
 @Entity
 public class Monitor extends GeneralData {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    private UUID id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column
     private LocalDateTime startDate;
+
+    @Column
     private LocalDateTime endDate;
 
-
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_monitor_id")
+    @JoinColumn(name = "user_monitor_id", nullable = false)
     private User userMonitor;
 
-    @OneToMany(cascade = CascadeType.REMOVE, fetch = FetchType.LAZY)
-    private List<Subject> subjects;
+    @OneToOne(cascade = CascadeType.ALL, mappedBy = "monitor")
+    private Monitoring monitoring;
+
 }
