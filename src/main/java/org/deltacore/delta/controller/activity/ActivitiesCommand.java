57f943c251831e4e9c02fddac8fdb5b4f62da07a
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-07 00:26:14 -0300

    feat: refactor saveActivity method and add uploadFiles endpoint for file handling

--- src/main/java/org/deltacore/delta/controller/activity/ActivitiesCommand.java
+++ src/main/java/org/deltacore/delta/controller/activity/ActivitiesCommand.java
@@ -1,9 +1,11 @@
 package org.deltacore.delta.controller.activity;
 
 import org.deltacore.delta.dto.ActivityDTO;
+import org.deltacore.delta.dto.ActivityFilesDTO;
 import org.deltacore.delta.dto.OnCreate;
 import org.deltacore.delta.dto.OnUpdate;
 import org.deltacore.delta.service.ActivitiesSectionService;
+import org.deltacore.delta.service.ActivityUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,38 +15,25 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/activities/monitor")
 public class ActivitiesCommand {
     private final ActivitiesSectionService activitiesService;
+    private final ActivityUploadService activityUploadService;
 
     @Autowired
-    public ActivitiesCommand(ActivitiesSectionService activitiesService) {
+    public ActivitiesCommand(ActivitiesSectionService activitiesService, ActivityUploadService activityUploadService) {
         this.activitiesService = activitiesService;
+        this.activityUploadService = activityUploadService;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> saveActivity(@RequestBody @Validated(OnCreate.class) ActivityDTO activity,
-                                          @RequestBody MultipartFile[] files) throws IOException {
-
-        ResponseEntity<String> UNSUPPORTED_MEDIA_TYPE = verifyFileCompatibility(files);
-        if (UNSUPPORTED_MEDIA_TYPE != null) return UNSUPPORTED_MEDIA_TYPE;
-
+                                          MultipartFile[] files) throws IOException {
         return ResponseEntity
-                .ok(activitiesService.saveActivity(activity, files));
-    }
-
-    private static ResponseEntity<String> verifyFileCompatibility(MultipartFile[] files) {
-        for (MultipartFile file : files) {
-            if (file.isEmpty()) continue;
-            String type = file.getContentType();
-            if (type == null || !FileType.contains(type)) {
-                return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-                        .body("Unsupported file type: " + file.getOriginalFilename());
-            }
-        }
-        return null;
+                .ok(activitiesService.saveActivity(activity));
     }
 
     @PutMapping(value = "/edit/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -68,4 +57,21 @@ public class ActivitiesCommand {
         ActivityDTO updatedActivity = activitiesService.completeActivity(id);
         return ResponseEntity.ok(updatedActivity);
     }
+
+    @PostMapping(value = "/files/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> uploadFiles(@RequestParam("file") MultipartFile[] files, @PathVariable Long id) throws IOException {
+
+        for (MultipartFile file : files) {
+            if(file.isEmpty()) continue;
+            String type = file.getContentType();
+            if (type == null || !FileType.contains(type)) {
+                return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                        .body("Unsupported file type: " + file.getOriginalFilename());
+            }
+        }
+
+        List<ActivityFilesDTO> metadataList = activityUploadService.uploadAndSaveFiles(files, id);
+        return ResponseEntity.ok(metadataList);
+    }
+
 }
