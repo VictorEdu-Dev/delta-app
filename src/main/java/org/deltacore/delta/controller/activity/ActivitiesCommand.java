fce94ebbabd784bf4ea4eb25b3c5ca4e9f6cf4cd
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-07 00:13:32 -0300

    feat: refactor ActivitiesCommand to integrate file compatibility verification and streamline activity saving

--- src/main/java/org/deltacore/delta/controller/activity/ActivitiesCommand.java
+++ src/main/java/org/deltacore/delta/controller/activity/ActivitiesCommand.java
@@ -1,11 +1,9 @@
 package org.deltacore.delta.controller.activity;
 
 import org.deltacore.delta.dto.ActivityDTO;
-import org.deltacore.delta.dto.ActivityFilesDTO;
 import org.deltacore.delta.dto.OnCreate;
 import org.deltacore.delta.dto.OnUpdate;
 import org.deltacore.delta.service.ActivitiesSectionService;
-import org.deltacore.delta.service.ActivityUploadService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,18 +19,33 @@ import java.util.List;
 @RequestMapping("/activities/monitor")
 public class ActivitiesCommand {
     private final ActivitiesSectionService activitiesService;
-    private final ActivityUploadService activityUploadService;
 
     @Autowired
-    public ActivitiesCommand(ActivitiesSectionService activitiesService, ActivityUploadService activityUploadService) {
+    public ActivitiesCommand(ActivitiesSectionService activitiesService) {
         this.activitiesService = activitiesService;
-        this.activityUploadService = activityUploadService;
     }
 
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> saveActivity(@RequestBody @Validated(OnCreate.class) ActivityDTO activity) {
+    public ResponseEntity<?> saveActivity(@RequestBody @Validated(OnCreate.class) ActivityDTO activity,
+                                          MultipartFile[] files) throws IOException {
+
+        ResponseEntity<String> UNSUPPORTED_MEDIA_TYPE = verifyFileCompatibility(files);
+        if (UNSUPPORTED_MEDIA_TYPE != null) return UNSUPPORTED_MEDIA_TYPE;
+
         return ResponseEntity
-                .ok(activitiesService.saveActivity(activity));
+                .ok(activitiesService.saveActivity(activity, files));
+    }
+
+    private static ResponseEntity<String> verifyFileCompatibility(MultipartFile[] files) {
+        for (MultipartFile file : files) {
+            if (file.isEmpty()) continue;
+            String type = file.getContentType();
+            if (type == null || !FileType.contains(type)) {
+                return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
+                        .body("Unsupported file type: " + file.getOriginalFilename());
+            }
+        }
+        return null;
     }
 
     @PutMapping(value = "/edit/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -56,29 +69,4 @@ public class ActivitiesCommand {
         ActivityDTO updatedActivity = activitiesService.completeActivity(id);
         return ResponseEntity.ok(updatedActivity);
     }
-
-    @PostMapping(value = "/files/{id}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> uploadFiles(@RequestParam("file") MultipartFile[] files, @PathVariable Long id) throws IOException {
-
-        List<String> allowedTypes = List.of(
-                "application/pdf",
-                "image/png",
-                "image/jpeg",
-                "application/msword",
-                "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-        );
-
-        for (MultipartFile file : files) {
-            if(file.isEmpty()) continue;
-            String type = file.getContentType();
-            if (type == null || !allowedTypes.contains(type)) {
-                return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE)
-                        .body("Unsupported file type: " + file.getOriginalFilename());
-            }
-        }
-
-        List<ActivityFilesDTO> metadataList = activityUploadService.uploadAndSaveFiles(files, id);
-        return ResponseEntity.ok(metadataList);
-    }
-
 }
