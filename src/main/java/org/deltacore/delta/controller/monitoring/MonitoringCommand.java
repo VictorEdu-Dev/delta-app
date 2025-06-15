5e55d33198d012aa1e274bb28b6219b7c75520dd
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-15 00:40:30 -0300

    feat: add MonitoringCommand for handling tutoring registration

---
+++ src/main/java/org/deltacore/delta/controller/monitoring/MonitoringCommand.java
@@ -0,0 +1,38 @@
+package org.deltacore.delta.controller.monitoring;
+
+import jakarta.validation.Valid;
+import org.deltacore.delta.dto.monitoring.TutoringDTO;
+import org.deltacore.delta.service.monitoring.MonitoringCommandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("tutoring/")
+public class MonitoringCommand {
+
+    private final MonitoringCommandService monitoringCmdService;
+
+    @Autowired
+    public MonitoringCommand(MonitoringCommandService monitoringCmdService) {
+        this.monitoringCmdService = monitoringCmdService;
+    }
+
+    @PostMapping(
+            path = "register",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> register(@RequestBody @Valid TutoringDTO monitoring) {
+        if (monitoring == null) return ResponseEntity.badRequest().body("Monitoring data is required");
+        TutoringDTO tutoringDTO = monitoringCmdService.registerMonitoring(monitoring);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(tutoringDTO);
+    }
+
+}
