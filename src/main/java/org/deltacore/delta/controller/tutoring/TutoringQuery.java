4232eaf89c20659a1babb50019b816d00d3e551b
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-15 00:56:05 -0300

    refactor: rename package from monitoring to tutoring in MonitoringQuery.java (#24)

--- src/main/java/org/deltacore/delta/controller/tutoring/TutoringQuery.java
+++ src/main/java/org/deltacore/delta/controller/tutoring/TutoringQuery.java
@@ -1,6 +1,6 @@
-package org.deltacore.delta.controller.monitoring;
+package org.deltacore.delta.controller.tutoring;
 
-import org.deltacore.delta.service.monitoring.MonitoringQueryService;
+import org.deltacore.delta.service.tutoring.TutoringQueryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -12,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("monitoring/")
-public class MonitoringQuery {
-    private final MonitoringQueryService monitoringQueryService;
+public class TutoringQuery {
+    private final TutoringQueryService tutoringQueryService;
 
     @Autowired
-    public MonitoringQuery(MonitoringQueryService monitoringQueryService) {
-        this.monitoringQueryService = monitoringQueryService;
+    public TutoringQuery(TutoringQueryService tutoringQueryService) {
+        this.tutoringQueryService = tutoringQueryService;
     }
 
     @GetMapping(path = "get/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -32,14 +32,14 @@ public class MonitoringQuery {
 
     @GetMapping(path = "subject/{code}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> findSubjectByCode(@PathVariable("code") String code) {
-        return monitoringQueryService.findSubjectByCode(code)
+        return tutoringQueryService.findSubjectByCode(code)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @GetMapping(path = "monitor/{username}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<?> findMonitorByUserUsername(@PathVariable("username") String username) {
-        return monitoringQueryService.findMonitorByUserUsername(username)
+        return tutoringQueryService.findMonitorByUserUsername(username)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
