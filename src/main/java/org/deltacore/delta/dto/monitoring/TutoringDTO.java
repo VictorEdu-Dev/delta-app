57dde5b784eb8b8235bd275c3901d92e2271c135
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-15 00:41:18 -0300

    refactor: add TutoringDTO class for structured tutoring data representation

---
+++ src/main/java/org/deltacore/delta/dto/monitoring/TutoringDTO.java
@@ -0,0 +1,44 @@
+package org.deltacore.delta.dto.monitoring;
+
+import jakarta.validation.constraints.*;
+import lombok.Builder;
+import org.deltacore.delta.dto.user.UserDTO;
+import org.deltacore.delta.model.monitoring.Modality;
+import org.hibernate.validator.constraints.URL;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+public record TutoringDTO(
+        Long id,
+
+        @Size(max = 150, message = "{tutoring.description.size}")
+        String description,
+
+        @Size(max = 1000, message = "{tutoring.urlThumbnail.size}")
+        @URL(message = "{tutoring.urlThumbnail.format}", regexp = "^(https?|ftp)://.*")
+        String urlThumbnail,
+
+        @Size(max = 50, message = "{tutoring.location.size}")
+        String location,
+
+        @NotNull(message = "{tutoring.vacancies.min}")
+        @Min(value = 1, message = "{tutoring.vacancies.min}")
+        Integer vacancies,
+
+
+        @GabeModality(message = "{tutoring.mode.compliant}")
+        String mode,
+
+        @NotNull(message = "{tutoring.is_active.not_null}")
+        Boolean isActive,
+
+        LocalDateTime createdAt,
+
+        SubjectDTO subject,
+        MonitorDTO monitor,
+        List<DayTimeEntryDTO> daysOfWeek,
+        List<UserDTO> users
+) {
+}
