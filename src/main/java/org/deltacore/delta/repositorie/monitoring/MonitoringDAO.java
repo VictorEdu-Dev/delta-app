2982020c1d11b4eaca658b141eba59a1405d0b90
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-15 00:40:41 -0300

    feat: add MonitoringDAO interface for Tutoring entity management

---
+++ src/main/java/org/deltacore/delta/repositorie/monitoring/MonitoringDAO.java
@@ -0,0 +1,7 @@
+package org.deltacore.delta.repositorie.monitoring;
+
+import org.deltacore.delta.model.monitoring.Tutoring;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MonitoringDAO extends CrudRepository<Tutoring, Long> {
+}
