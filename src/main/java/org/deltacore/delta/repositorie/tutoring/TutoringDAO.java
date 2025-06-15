cd6cd3209e269bd754159224a2a9ac7ee75719a1
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-15 00:55:48 -0300

    feat: add TutoringDAO interface for tutoring data access (#24)

---
+++ src/main/java/org/deltacore/delta/repositorie/tutoring/TutoringDAO.java
@@ -0,0 +1,7 @@
+package org.deltacore.delta.repositorie.tutoring;
+
+import org.deltacore.delta.model.tutoring.Tutoring;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TutoringDAO extends CrudRepository<Tutoring, Long> {
+}
