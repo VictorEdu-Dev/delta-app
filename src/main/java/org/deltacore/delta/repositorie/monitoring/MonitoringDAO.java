23a148122b34e1b08a8813699c202787ffdf9155
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-15 00:54:55 -0300

    refactor: rename

--- src/main/java/org/deltacore/delta/repositorie/monitoring/MonitoringDAO.java
+++
@@ -1,7 +0,0 @@
-package org.deltacore.delta.repositorie.monitoring;
-
-import org.deltacore.delta.model.monitoring.Tutoring;
-import org.springframework.data.repository.CrudRepository;
-
-public interface MonitoringDAO extends CrudRepository<Tutoring, Long> {
-}
