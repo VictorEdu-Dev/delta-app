0c59dec381cbdf38bae5c16665a03a99ad526a79
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-15 21:13:10 -0300

    refactor: update DeltaUserDetailsService to return DeltaUserDetails and improve user retrieval logic (#27)

--- src/main/java/org/deltacore/delta/config/security/DeltaUserDetailsService.java
+++ src/main/java/org/deltacore/delta/config/security/DeltaUserDetailsService.java
@@ -5,10 +5,11 @@ import org.deltacore.delta.dto.user.UserDeltaMapper;
 import org.deltacore.delta.model.user.User;
 import org.deltacore.delta.repositorie.user.UserDAO;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class DeltaUserDetailsService implements UserDetailsService {
@@ -22,11 +23,11 @@ public class DeltaUserDetailsService implements UserDetailsService {
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userDAO.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    public DeltaUserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userDAO.findByUsername(username);
 
-        UserDTO userDTO = userDeltaMapper.toDTO(user);
+        UserDTO userDTO = userDeltaMapper.toDTONoLazyRelationship(user.orElseThrow(() ->
+                new UsernameNotFoundException("User not found with username: " + username)));
         return new DeltaUserDetails(userDTO);
     }
 }
