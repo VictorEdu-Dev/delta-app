60cffd0ea608c356b18ea2fab2d7294894fe2e3b
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-15 21:13:53 -0300

    feat: enhance UserDTO with additional fields for improved user data representation (#27)

--- src/main/java/org/deltacore/delta/dto/user/UserDTO.java
+++ src/main/java/org/deltacore/delta/dto/user/UserDTO.java
@@ -2,7 +2,11 @@ package org.deltacore.delta.dto.user;
 
 import jakarta.validation.constraints.*;
 import lombok.Builder;
+import org.deltacore.delta.dto.tutoring.TutoringDTO;
 import org.deltacore.delta.model.user.Roles;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder(toBuilder = true)
 public record UserDTO(
@@ -16,6 +20,7 @@ public record UserDTO(
         )
         String username,
 
+        @Size(max = 255)
         @Pattern(
                 regexp = "^[a-zA-Z0-9._%+-]{1,64}@[a-zA-Z0-9.-]{1,255}\\.[a-zA-Z]{2,}$",
                 message = "{user.email.format}"
@@ -23,13 +28,19 @@ public record UserDTO(
         String email,
 
         @NotBlank(message = "{user.password.notblank}")
-        @Size(min = 6, message = "{user.password.size}")
+        @Size(min = 6, max = 70, message = "{user.password.size}")
         @Pattern(regexp = ".*[A-Z].*", message = "{user.password.pattern.uppercase}")
         @Pattern(regexp = ".*[a-z].*", message = "{user.password.pattern.lowercase}")
         @Pattern(regexp = ".*\\d.*", message = "{user.password.pattern.digit}")
         @Pattern(regexp = ".*[^a-zA-Z0-9].*", message = "{user.password.pattern.special}")
         String passwordHash,
 
-        Roles role
+        LocalDateTime createdAt,
+
+        Roles role,
+
+        ProfileDTO profile,
+
+        List<TutoringDTO>tutorings
 ) {}
 
