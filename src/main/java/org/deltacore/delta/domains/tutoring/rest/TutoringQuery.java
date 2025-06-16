5fed9626498e5d7d59b3d924c844ccee3c55d25b
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-15 22:43:09 -0300

    refactor: update request mapping in TutoringQuery to reflect new endpoint structure (#28)

--- src/main/java/org/deltacore/delta/domains/tutoring/rest/TutoringQuery.java
+++ src/main/java/org/deltacore/delta/domains/tutoring/rest/TutoringQuery.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("monitoring/")
+@RequestMapping("tutoring/")
 public class TutoringQuery {
     private final TutoringQueryService tutoringQueryService;
 
