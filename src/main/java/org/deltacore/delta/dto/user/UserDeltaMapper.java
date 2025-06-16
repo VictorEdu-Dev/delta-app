ca8335cc7068f593c1c802c896b4ce419b6df4dc
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-15 21:13:48 -0300

    feat: add mapping methods to UserDeltaMapper for improved DTO handling (#27)

--- src/main/java/org/deltacore/delta/dto/user/UserDeltaMapper.java
+++ src/main/java/org/deltacore/delta/dto/user/UserDeltaMapper.java
@@ -2,14 +2,21 @@ package org.deltacore.delta.dto.user;
 
 import org.deltacore.delta.model.user.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface UserDeltaMapper {
 
     User toEntity(UserDTO userDTO);
 
+    @Named("fullMapping")
     UserDTO toDTO(User user);
+
+    @Named("noLazy")
+    @Mapping(target = "tutorings", ignore = true)
+    UserDTO toDTONoLazyRelationship(User user);
 
     void updateEntityFromDTO(UserDTO userDTO, @MappingTarget User user);
 }
