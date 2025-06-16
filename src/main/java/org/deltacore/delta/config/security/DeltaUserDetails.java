341c25eb0b25ae6cb8af250479701ee897c4cc20
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-15 21:13:04 -0300

    refactor: update DeltaUserDetails to use UserDTO and simplify role access (#27)

--- src/main/java/org/deltacore/delta/config/security/DeltaUserDetails.java
+++ src/main/java/org/deltacore/delta/config/security/DeltaUserDetails.java
@@ -1,48 +1,40 @@
 package org.deltacore.delta.config.security;
 
 import org.deltacore.delta.dto.user.UserDTO;
-import org.deltacore.delta.dto.user.UserDeltaMapper;
-import org.deltacore.delta.model.user.Permissions;
 import org.deltacore.delta.model.user.Roles;
-import org.deltacore.delta.model.user.User;
-import org.mapstruct.factory.Mappers;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-
 import java.util.Collection;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class DeltaUserDetails implements UserDetails {
 
-    private final User user;
+    private final UserDTO user;
 
     public DeltaUserDetails(UserDTO userDTO) {
-        UserDeltaMapper userDeltaMapper = Mappers.getMapper(UserDeltaMapper.class);
-        this.user = userDeltaMapper.toEntity(userDTO);
+        this.user = userDTO;
     }
 
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        Roles role = user.getRole();
+        Roles role = user.role();
         return List.of(new SimpleGrantedAuthority("ROLE_" + role.name()));
     }
 
     public String getPassword() {
-        return user.getPasswordHash();
+        return user.passwordHash();
     }
 
     public String getUsername() {
-        return user.getUsername();
+        return user.username();
     }
 
     public String getEmail() {
-        return user.getEmail();
+        return user.email();
     }
 
     public Roles getRole() {
-        return user.getRole();
+        return user.role();
     }
 }
