a39b86006ad743053e993ae44f199d571779c50f
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-18 16:30:48 -0300

    fix: update request mapping in AuthQuery to /auth for improved clarity (#27)

--- src/main/java/org/deltacore/delta/domains/auth/rest/AuthQuery.java
+++ src/main/java/org/deltacore/delta/domains/auth/rest/AuthQuery.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(path = "/home")
+@RequestMapping(path = "/auth")
 public class AuthQuery {
 
     private final UserQueryService userQueryService;
