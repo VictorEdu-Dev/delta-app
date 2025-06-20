5f2250491ad9eedbb25b0e1e99c3dc264dbc0822
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-20 19:09:42 -0300

    fix: mark JwtConfig class as deprecated for future removal (#31)

--- src/main/java/org/deltacore/delta/core/config/security/JwtConfig.java
+++ src/main/java/org/deltacore/delta/core/config/security/JwtConfig.java
@@ -1,41 +1,8 @@
 package org.deltacore.delta.core.config.security;
 
-import jakarta.annotation.PostConstruct;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-
-@Configuration
+@Deprecated(
+        since = "0.1.0",
+        forRemoval = true)
 public class JwtConfig {
-    @Value("${jwt.secret}")
-    private String jwtSecret;
-    private SecretKeySpec secretKey;
-
-    @PostConstruct
-    public void init() {
-        this.secretKey = new SecretKeySpec(jwtSecret.getBytes(StandardCharsets.UTF_8), "HMACSHA256");
-    }
-
-    @Bean
-    public JwtDecoder jwtDecoder() {
-        return NimbusJwtDecoder.withSecretKey(this.secretKey).build();
-    }
-
-    @Bean
-    public JwtAuthenticationConverter jwtAuthenticationConverter() {
-        JwtGrantedAuthoritiesConverter converter = new JwtGrantedAuthoritiesConverter();
-        converter.setAuthoritiesClaimName("role");
-        converter.setAuthorityPrefix("");
-        JwtAuthenticationConverter jwtAuthConverter = new JwtAuthenticationConverter();
-        jwtAuthConverter.setJwtGrantedAuthoritiesConverter(converter);
-        return jwtAuthConverter;
-    }
-
 }
