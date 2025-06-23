f7cb7ba934c474ed64ef25bc57932a9f53cddc09
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-22 21:15:40 -0300

    feat: add Swagger annotations to SubjectDTO for improved API documentation

--- src/main/java/org/deltacore/delta/domains/tutoring/dto/SubjectDTO.java
+++ src/main/java/org/deltacore/delta/domains/tutoring/dto/SubjectDTO.java
@@ -1,5 +1,6 @@
 package org.deltacore.delta.domains.tutoring.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import org.deltacore.delta.domains.activity.dto.ActivityDTO;
 
 import java.util.List;
@@ -9,7 +10,9 @@ public record SubjectDTO(
         String code,
         String name,
         Boolean isActive,
+        @Schema(hidden = true)
         List<ActivityDTO> activities,
+        @Schema(hidden = true)
         TutoringDTO monitoring
 ) {
 }
