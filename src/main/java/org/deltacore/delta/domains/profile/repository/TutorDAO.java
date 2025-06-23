8b53c6618d25ee3049c907b5ec37f7f3ed703298
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-22 21:15:45 -0300

    feat: update SQL query in TutorDAO for improved clarity and add method to retrieve active/inactive tutors

--- src/main/java/org/deltacore/delta/domains/profile/repository/TutorDAO.java
+++ src/main/java/org/deltacore/delta/domains/profile/repository/TutorDAO.java
@@ -5,9 +5,18 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TutorDAO extends CrudRepository<Tutor,Long> {
-    @Query(value = "SELECT mo.* FROM monitor mo JOIN user_delta us ON mo.user_monitor_id = us.id WHERE us.username = :username", nativeQuery = true)
+
+    @Query(value = "SELECT t.* " +
+            "FROM monitor t " +
+            "JOIN user_monitor u ON t.user_monitor_id = u.id " +
+            "WHERE u.username = :username ",
+            nativeQuery = true)
     Optional<Tutor> findByUserUsername(@Param("username") String username);
+
+    @Query(value = "SELECT * FROM monitor WHERE is_active = :condition", nativeQuery = true)
+    List<Tutor> findAllActiveOrInactiveTutors(@Param("condition") Boolean condition);
 }
