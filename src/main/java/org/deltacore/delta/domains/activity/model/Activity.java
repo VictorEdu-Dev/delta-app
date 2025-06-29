5dc737ff8c5d90984f5c24b61e0f3b4d49c221d6
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-29 17:33:42 -0300

    feat: remove custom equals and hashCode methods from Activity entity for simplification

--- src/main/java/org/deltacore/delta/domains/activity/model/Activity.java
+++ src/main/java/org/deltacore/delta/domains/activity/model/Activity.java
@@ -70,23 +70,4 @@ public class Activity extends GeneralData {
     @Fetch(FetchMode.JOIN)
     @ToString.Exclude
     private List<ActivityFiles> files;
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Activity activity = (Activity) o;
-        return getId() != null && Objects.equals(getId(), activity.getId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return getId() != null ? getId().hashCode() :
-                (this instanceof HibernateProxy
-                        ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode()
-                        : getClass().hashCode());
-    }
 }
