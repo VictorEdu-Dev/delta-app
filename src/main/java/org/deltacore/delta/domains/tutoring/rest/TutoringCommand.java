73e4f8d05e7b790cee0a93deafc4c5b0c43344a0
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-29 18:53:23 -0300

    feat: enhance TutoringCommand with update and deactivate endpoints for improved functionality (#32)

--- src/main/java/org/deltacore/delta/domains/tutoring/rest/TutoringCommand.java
+++ src/main/java/org/deltacore/delta/domains/tutoring/rest/TutoringCommand.java
@@ -3,14 +3,13 @@ package org.deltacore.delta.domains.tutoring.rest;
 import jakarta.validation.Valid;
 import org.deltacore.delta.domains.tutoring.dto.TutoringDTO;
 import org.deltacore.delta.domains.tutoring.servive.TutoringCommandService;
+import org.deltacore.delta.shared.dto.OnCreate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController()
 @RequestMapping("/tutoring")
@@ -27,11 +26,32 @@ public class TutoringCommand {
             path = "/register",
             produces = MediaType.APPLICATION_JSON_VALUE,
             consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> register(@RequestBody @Valid TutoringDTO monitoring) {
+    public ResponseEntity<?> register(@RequestBody @Validated(OnCreate.class) TutoringDTO monitoring) {
         TutoringDTO tutoringDTO = monitoringCmdService.registerMonitoring(monitoring);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(tutoringDTO);
+    }
+
+    @PatchMapping(
+            path = "/update",
+            produces = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> update(@RequestBody @Valid TutoringDTO monitoring) {
+        TutoringDTO updated = monitoringCmdService.updateMonitoring(monitoring);
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/deactivate")
+    public ResponseEntity<?> deactivate() {
+        monitoringCmdService.deactivateTutoring();
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/reactivate")
+    public ResponseEntity<?> reactivate() {
+        monitoringCmdService.reactivateTutoring();
+        return ResponseEntity.noContent().build();
     }
 
 }
