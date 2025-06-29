f42a6ad50ba9ceb9afab61d42f1ec8022869f807
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-29 18:53:39 -0300

    feat: add updateEntityFromDto method to TutoringMapper for improved entity updates (#32)

--- src/main/java/org/deltacore/delta/domains/tutoring/dto/TutoringMapper.java
+++ src/main/java/org/deltacore/delta/domains/tutoring/dto/TutoringMapper.java
@@ -2,7 +2,10 @@ package org.deltacore.delta.domains.tutoring.dto;
 
 import org.deltacore.delta.domains.profile.dto.UserMapper;
 import org.deltacore.delta.domains.tutoring.model.Tutoring;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring",
         uses = {
@@ -14,4 +17,6 @@ import org.mapstruct.Mapper;
 public interface TutoringMapper {
     Tutoring toEntity(TutoringDTO tutoringDTO);
     TutoringDTO toDTO(Tutoring tutoring);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromDto(TutoringDTO dto, @MappingTarget Tutoring tutoring);
 }
