d7edb21a638648cedac18c09a135d908b8b207ff
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-29 18:53:19 -0300

    feat: add exception handler for TutoringNotFound in TutoringAdvice for improved error handling (#32)

--- src/main/java/org/deltacore/delta/domains/tutoring/rest/TutoringAdvice.java
+++ src/main/java/org/deltacore/delta/domains/tutoring/rest/TutoringAdvice.java
@@ -1,6 +1,7 @@
 package org.deltacore.delta.domains.tutoring.rest;
 
 import org.deltacore.delta.domains.tutoring.exception.ConflictException;
+import org.deltacore.delta.domains.tutoring.exception.TutoringNotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -14,6 +15,13 @@ public class TutoringAdvice {
     public ResponseEntity<?> handleBusiness(ConflictException ex) {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
+                .body(Map.of("Error:", ex.getMessage()));
+    }
+
+    @ExceptionHandler(TutoringNotFound.class)
+    public ResponseEntity<?> handleBusiness(TutoringNotFound ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
                 .body(Map.of("Error:", ex.getMessage()));
     }
 }
