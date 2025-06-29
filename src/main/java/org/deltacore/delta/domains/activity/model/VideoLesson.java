5fe1fc80565e6cd80eb2b8bee2dd6b3ff24794ea
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-29 17:36:31 -0300

    feat: remove custom equals and hashCode methods from VideoLesson class for simplicity

--- src/main/java/org/deltacore/delta/domains/activity/model/VideoLesson.java
+++ src/main/java/org/deltacore/delta/domains/activity/model/VideoLesson.java
@@ -40,20 +40,4 @@ public class VideoLesson extends GeneralData {
     @JoinColumn(name = "activity_id")
     @ToString.Exclude
     private Activity activity;
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        VideoLesson that = (VideoLesson) o;
-        return getId() != null && Objects.equals(getId(), that.getId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
