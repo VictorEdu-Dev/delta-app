8536bb5e46a6644d9ede9d59ca992be2f4b45b46
Victor Eduardo <victoreduardodev@gmail.com>
2025-06-29 17:35:43 -0300

    feat: remove custom equals/hashCode methods from Tutor entity for improved consistency (#24)

--- src/main/java/org/deltacore/delta/domains/profile/model/Tutor.java
+++ src/main/java/org/deltacore/delta/domains/profile/model/Tutor.java
@@ -37,20 +37,4 @@ public class Tutor extends GeneralData {
 
     @OneToOne(cascade = CascadeType.ALL, mappedBy = "monitor")
     private Tutoring tutoring;
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        Tutor tutor = (Tutor) o;
-        return getId() != null && Objects.equals(getId(), tutor.getId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
-    }
 }
