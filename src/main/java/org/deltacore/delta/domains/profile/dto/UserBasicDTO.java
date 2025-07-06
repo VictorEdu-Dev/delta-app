537cd7a1e611cff69b94e898b20a80c14209908d
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-06 16:41:40 -0300

    feat: add field role user at dto

--- src/main/java/org/deltacore/delta/domains/profile/dto/UserBasicDTO.java
+++ src/main/java/org/deltacore/delta/domains/profile/dto/UserBasicDTO.java
@@ -1,7 +1,10 @@
 package org.deltacore.delta.domains.profile.dto;
 
+import org.deltacore.delta.domains.profile.model.Roles;
+
 public record UserBasicDTO(
         Long id,
-        String username
+        String username,
+        Roles role
 ) {
 }
