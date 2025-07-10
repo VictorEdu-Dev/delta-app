11222ca91256df65b93091a793be3afa8db12f41
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-09 22:19:24 -0300

    feat: add AccountQuery controller for profile account management (#33)

---
+++ src/main/java/org/deltacore/delta/domains/profile/rest/AccountQuery.java
@@ -0,0 +1,9 @@
+package org.deltacore.delta.domains.profile.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/account/profile")
+public class AccountQuery {
+}
