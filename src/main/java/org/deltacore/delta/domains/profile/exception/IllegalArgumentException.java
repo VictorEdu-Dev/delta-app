1c0610374c000b6811f6f49ed9a771ff9a3b1642
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-09 22:19:40 -0300

    feat: add custom IllegalArgumentException for profile management (#33)

---
+++ src/main/java/org/deltacore/delta/domains/profile/exception/IllegalArgumentException.java
@@ -0,0 +1,7 @@
+package org.deltacore.delta.domains.profile.exception;
+
+public class IllegalArgumentException extends RuntimeException {
+    public IllegalArgumentException(String message) {
+        super(message);
+    }
+}
