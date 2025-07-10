fd14882251a1f583327130b27bdbe7a260ac3907
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-09 22:20:07 -0300

    feat: add ProfileNotFound exception for profile management (#33)

---
+++ src/main/java/org/deltacore/delta/domains/profile/exception/ProfileNotFound.java
@@ -0,0 +1,7 @@
+package org.deltacore.delta.domains.profile.exception;
+
+public class ProfileNotFound extends RuntimeException {
+    public ProfileNotFound(String message) {
+        super(message);
+    }
+}
