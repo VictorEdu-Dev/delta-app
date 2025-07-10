bc49279380930b9f9386d5aa4700a31269fd2428
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-09 22:19:12 -0300

    feat: add exception handlers for IllegalArgumentException and ProfileNotFound (#33)

--- src/main/java/org/deltacore/delta/domains/profile/rest/AccountAdvice.java
+++ src/main/java/org/deltacore/delta/domains/profile/rest/AccountAdvice.java
@@ -2,6 +2,8 @@ package org.deltacore.delta.domains.profile.rest;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.deltacore.delta.domains.profile.exception.ConflictException;
+import org.deltacore.delta.domains.profile.exception.IllegalArgumentException;
+import org.deltacore.delta.domains.profile.exception.ProfileNotFound;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -15,11 +17,25 @@ import java.util.Map;
 public class AccountAdvice {
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<Map<String, Object>> handleConflictException(ConflictException ex, HttpServletRequest request) {
+        return getMapResponseEntity(request, ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, Object>> handleConflictException(IllegalArgumentException ex, HttpServletRequest request) {
+        return getMapResponseEntity(request, ex.getMessage());
+    }
+
+    @ExceptionHandler(ProfileNotFound.class)
+    public ResponseEntity<Map<String, Object>> handleConflictException(ProfileNotFound ex, HttpServletRequest request) {
+        return getMapResponseEntity(request, ex.getMessage());
+    }
+
+    private ResponseEntity<Map<String, Object>> getMapResponseEntity(HttpServletRequest request, String message) {
         Map<String, Object> body = new LinkedHashMap<>();
         body.put("timestamp", Instant.now().toString());
         body.put("status", HttpStatus.CONFLICT.value());
         body.put("error", "Conflict");
-        body.put("message", ex.getMessage());
+        body.put("message", message);
         body.put("path", request.getRequestURI());
 
         return ResponseEntity.status(HttpStatus.CONFLICT).body(body);
