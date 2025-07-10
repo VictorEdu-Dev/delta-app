a374df919e72263e28010781ea4d59739a2871b1
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-09 22:19:51 -0300

    feat: update ProfileDAO to use named parameter for findByUserUsername method (#33)

--- src/main/java/org/deltacore/delta/domains/profile/repository/ProfileDAO.java
+++ src/main/java/org/deltacore/delta/domains/profile/repository/ProfileDAO.java
@@ -3,10 +3,9 @@ package org.deltacore.delta.domains.profile.repository;
 import org.deltacore.delta.domains.profile.model.Profile;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ProfileDAO extends CrudRepository<Profile, Long> {
-    @Query("SELECT p FROM Profile p JOIN FETCH p.user u WHERE u.username = ?1")
-    Optional<Profile> findByUserUsername(String username);
 }
