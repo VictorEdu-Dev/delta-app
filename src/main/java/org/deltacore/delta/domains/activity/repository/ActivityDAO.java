b11a17af1178b58e566c74fbee57d08a396f9153
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-14 16:06:26 -0300

    feat: fix SQL query syntax for title matching in ActivityDAO

--- src/main/java/org/deltacore/delta/domains/activity/repository/ActivityDAO.java
+++ src/main/java/org/deltacore/delta/domains/activity/repository/ActivityDAO.java
@@ -22,10 +22,10 @@ public interface ActivityDAO extends CrudRepository<Activity, Long>, JpaSpecific
     @Query(value = "SELECT * FROM activity WHERE LOWER(TRIM(title)) LIKE LOWER(CONCAT('%', ?1, '%')) LIMIT 10", nativeQuery = true)
     Iterable<Activity> findActivitiesByTitle(String title);
 
-    @Query(value = "SELECT * FROM activity WHERE title LIKE %?1% AND activityType = ?2 LIMIT ?3", nativeQuery = true)
+    @Query(value = "SELECT * FROM activity WHERE title LIKE '%?1%' AND activityType = ?2 LIMIT ?3", nativeQuery = true)
     Iterable<Activity> findActivitiesByTitleAndType(String title, ActivityType type, Integer limit);
 
-    @Query(value = "SELECT * FROM activity WHERE title LIKE %?1% AND activityType = ?2 LIMIT 10", nativeQuery = true)
+    @Query(value = "SELECT * FROM activity WHERE title LIKE '%?1%' AND activityType = ?2 LIMIT 10", nativeQuery = true)
     Iterable<Activity> findActivitiesByTitleAndType(String title, ActivityType type);
 
     @Query(value = "SELECT * FROM activity WHERE activityType = ?1 LIMIT ?2", nativeQuery = true)
@@ -57,7 +57,7 @@ public interface ActivityDAO extends CrudRepository<Activity, Long>, JpaSpecific
     @Query(value = "SELECT title FROM activity WHERE title = ?1", nativeQuery = true)
     Optional<String> findActByTitle(String title);
 
-    @Query(value = "SELECT new org.deltacore.delta.domains.activity.dto.ActivityMiniatureDTO(u.id, u.title, u.status, u.activityType, u.maxScore, u.recommendedLevel, u.deadline) FROM Activity u " +
+    @Query(value = "SELECT new org.deltacore.delta.domains.activity.dto.ActivityMiniatureDTO(u.id, u.title, u.status, u.activityType, u.maxScore, u.imageUrl, u.recommendedLevel, u.deadline) FROM Activity u " +
                     "WHERE (:status IS NULL OR u.status = :status) " +
                     "AND (:activityType IS NULL OR u.activityType = :activityType) ")
     Page<ActivityMiniatureDTO> findActivitiesMiniature(@Param("status") ActivityStatus status,
