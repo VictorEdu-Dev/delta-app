b2a33f12fd5e9282ac691ddbdd83807439586355
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-20 19:23:30 -0300

    feat: update ActivityDTO to rename recommendedLevel to difficultyLevel and add ActivityRegister record with validation

--- src/main/java/org/deltacore/delta/domains/activity/dto/ActivityDTO.java
+++ src/main/java/org/deltacore/delta/domains/activity/dto/ActivityDTO.java
@@ -1,13 +1,16 @@
 package org.deltacore.delta.domains.activity.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Builder;
 import org.deltacore.delta.domains.activity.model.ActivityStatus;
 import org.deltacore.delta.domains.activity.model.ActivityType;
 import org.deltacore.delta.shared.dto.OnCreate;
+import org.hibernate.validator.constraints.Range;
 import org.hibernate.validator.constraints.URL;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -27,7 +30,7 @@ public record ActivityDTO(
         String imageUrl,
 
         @Min(value = 1, message = "{activity.recommended_level.min}")
-        Integer recommendedLevel,
+        Integer difficultyLevel,
 
         @NotNull(message = "{activity.max_score.not.null}")
         @DecimalMin(value = "0.0", inclusive = false, message = "{activity.max_score.min}")
@@ -39,7 +42,29 @@ public record ActivityDTO(
         LocalDateTime deadline,
 
         boolean completed,
-        LocalDateTime completionTimestamp,
+        LocalDateTime completionTimestamp
+) {
+        @Builder(toBuilder = true)
+        public record ActivityRegister(
+                @Schema(hidden = true)
+                Long id,
 
-        List<ActivityFilesDTO> activityFilesDTO
-) {}
+                @NotBlank(message = "{activity.title.not.blank}")
+                String title,
+
+                @NotBlank(message = "{activity.description.not.blank}")
+                String description,
+
+                @NotNull(message = "{activity.activity_type.not.null}")
+                ActivityType activityType,
+
+                @Range(min = 1, max = 3, message = "{activity.difficulty_level.range}")
+                Integer difficultyLevel,
+
+                @FutureOrPresent(message = "{activity.deadline.future_or_present}", groups = OnCreate.class)
+                LocalDate deadline,
+
+                @NotBlank(message = "{activity.subject.not.blank}")
+                String subjectCode
+        ) {}
+}
