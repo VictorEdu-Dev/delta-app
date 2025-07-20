b6f48595f08938f8eb71cefbd7f5c34cb69d550e
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-20 19:23:15 -0300

    feat: refactor Activity class to improve field naming and add default value methods

--- src/main/java/org/deltacore/delta/domains/activity/model/Activity.java
+++ src/main/java/org/deltacore/delta/domains/activity/model/Activity.java
@@ -4,14 +4,9 @@ import jakarta.persistence.*;
 import lombok.*;
 import org.deltacore.delta.domains.tutoring.model.Subject;
 import org.deltacore.delta.shared.model.GeneralData;
-import org.hibernate.annotations.Fetch;
-import org.hibernate.annotations.FetchMode;
-import org.hibernate.proxy.HibernateProxy;
-
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Objects;
 
 @Getter
 @Setter
@@ -38,35 +33,72 @@ public class Activity extends GeneralData {
     @Column(length = 1000)
     private String imageUrl;
 
-    @Column(nullable = false)
-    private Integer recommendedLevel;
+    @Column(name = "difficulty_level", nullable = false)
+    private Integer difficultyLevel;
 
+    @Setter(AccessLevel.NONE)
     @Column(precision = 19, scale = 4)
     private BigDecimal maxScore;
 
     @Column(nullable = false)
     private LocalDateTime deadline;
 
+    @Setter(AccessLevel.NONE)
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private ActivityStatus status;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "activity")
-    @ToString.Exclude
-    private List<VideoLesson> videoUrl;
+    @Setter(AccessLevel.NONE)
+    @Column(name = "complete", nullable = false)
+    private boolean completed;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @Setter(AccessLevel.NONE)
+    @Column(name = "timestamp")
+    private LocalDateTime completionTimestamp;
+
+    @Setter(AccessLevel.NONE)
+    @Column(name = "last_access")
+    private LocalDateTime lastAccess;
+
+    @ManyToOne
     @JoinColumn(name = "subject_id")
     @ToString.Exclude
     private Subject subject;
 
-    @Column(name = "complete", nullable = false)
-    private boolean completed;
-
-    @Column(name = "timestamp")
-    private LocalDateTime completionTimestamp;
-
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY, mappedBy = "activity")
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "activity", orphanRemoval = true)
     @ToString.Exclude
     private List<ActivityFiles> files;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "activity", orphanRemoval = true)
+    @ToString.Exclude
+    private List<VideoLesson> videoUrl;
+
+    public void markAsCompleted() {
+        this.completed = true;
+        this.completionTimestamp = LocalDateTime.now();
+        this.status = ActivityStatus.COMPLETED;
+    }
+
+    public void setDefaultValues() {
+        if (this.deadline == null || this.deadline.isBefore(LocalDateTime.now()))
+            this.deadline = LocalDateTime.now().plusDays(7);
+        this.status = ActivityStatus.PENDING;
+    }
+
+    public void setMaxScore() {
+        this.maxScore = switch (this.difficultyLevel) {
+            case 1 -> new BigDecimal(20);
+            case 2 -> new BigDecimal(25);
+            case 3 -> new BigDecimal(30);
+            default -> BigDecimal.ZERO;
+        };
+    }
+
+    public void setLastAccess() {
+        this.lastAccess = LocalDateTime.now();
+    }
+
+    public void setImageUrl() {
+        this.imageUrl = "https://cdn.pixabay.com/photo/2020/09/23/03/54/background-5594879_1280.jpg";
+    }
 }
