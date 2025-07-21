6ecac30c2ef0d26b42eaf46d044c733a0fc92a1a
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-21 11:58:37 -0300

    feat: add EntityGraph to TutoringDAO for optimized fetching of related entities

--- src/main/java/org/deltacore/delta/domains/tutoring/repository/TutoringDAO.java
+++ src/main/java/org/deltacore/delta/domains/tutoring/repository/TutoringDAO.java
@@ -1,7 +1,14 @@
 package org.deltacore.delta.domains.tutoring.repository;
 
 import org.deltacore.delta.domains.tutoring.model.Tutoring;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface TutoringDAO extends CrudRepository<Tutoring, Long> {
+
+    @EntityGraph(attributePaths = {"monitor", "users", "daysOfWeek"})
+    Optional<Tutoring> findByMonitor_UserMonitor_Username(String username);
 }
