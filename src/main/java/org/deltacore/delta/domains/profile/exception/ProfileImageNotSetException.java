09532c8516a6c58e88f3aa376560466957fe2986
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-22 20:41:07 -0300

    feat: create ProfileImageNotSetException for handling unset profile images

---
+++ src/main/java/org/deltacore/delta/domains/profile/exception/ProfileImageNotSetException.java
@@ -0,0 +1,7 @@
+package org.deltacore.delta.domains.profile.exception;
+
+public class ProfileImageNotSetException extends RuntimeException {
+    public ProfileImageNotSetException(String message) {
+        super(message);
+    }
+}
