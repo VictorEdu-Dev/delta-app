a2b132bfbb4a23d6a5e8e749e64b123297656471
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-22 12:16:49 -0300

    feat: remove unused method for fetching user with tutorings from UserDAO

--- src/main/java/org/deltacore/delta/domains/profile/repository/UserDAO.java
+++ src/main/java/org/deltacore/delta/domains/profile/repository/UserDAO.java
@@ -3,16 +3,12 @@ package org.deltacore.delta.domains.profile.repository;
 import org.deltacore.delta.domains.profile.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 
 public interface UserDAO extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
-
-    @Query("SELECT u FROM user_delta u LEFT JOIN FETCH u.tutorings WHERE u.username = :username")
-    Optional<User> findByUsernameWithTutorings(@Param("username") String username);
 
     @Query("SELECT u.id FROM user_delta u WHERE u.username = :username")
     Optional<Long> findIdByUsername(String username);
