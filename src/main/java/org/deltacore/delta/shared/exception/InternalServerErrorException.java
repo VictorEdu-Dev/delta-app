98a9e4b34d702ab93f4bdba99055c997e91777fa
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-22 19:02:46 -0300

    feat: add InternalServerErrorException for handling internal server errors

---
+++ src/main/java/org/deltacore/delta/shared/exception/InternalServerErrorException.java
@@ -0,0 +1,11 @@
+package org.deltacore.delta.shared.exception;
+
+public class InternalServerErrorException extends RuntimeException {
+    public InternalServerErrorException(String message) {
+        super(message);
+    }
+
+    public InternalServerErrorException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
