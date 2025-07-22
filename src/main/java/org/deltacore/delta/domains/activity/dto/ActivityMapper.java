c0b9fc0c1b13d886e514b2e8515797c9430bbc14
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-22 13:45:55 -0300

    feat: update ActivityMapper to ignore links and files in toDTO mapping

--- src/main/java/org/deltacore/delta/domains/activity/dto/ActivityMapper.java
+++ src/main/java/org/deltacore/delta/domains/activity/dto/ActivityMapper.java
@@ -2,10 +2,13 @@ package org.deltacore.delta.domains.activity.dto;
 
 import org.deltacore.delta.domains.activity.model.Activity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring", uses = {LinkActivityMapper.class, ActivityFilesMapper.class})
 public interface ActivityMapper {
+    @Mapping(target = "links", ignore = true)
+    @Mapping(target = "files", ignore = true)
     ActivityDTO toDTO(Activity activity);
 
     Activity toEntity(ActivityDTO.ActivityRegister activityDTO);
