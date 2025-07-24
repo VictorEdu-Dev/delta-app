9b905a25efd1f3f161ac2961fdad3b3c04567a12
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-24 14:59:03 -0300

    feat: extend AuthExceptionAdvice to handle InvalidRecoveryCodeException

--- src/main/java/org/deltacore/delta/domains/auth/rest/AuthExceptionAdvice.java
+++ src/main/java/org/deltacore/delta/domains/auth/rest/AuthExceptionAdvice.java
@@ -1,19 +1,25 @@
 package org.deltacore.delta.domains.auth.rest;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.deltacore.delta.domains.auth.exception.InvalidRecoveryCodeException;
 import org.deltacore.delta.domains.auth.exception.InvalidTokenException;
 import org.deltacore.delta.domains.auth.exception.UserAlreadyExists;
+import org.deltacore.delta.domains.profile.exception.ProfileImageNotSetException;
 import org.deltacore.delta.domains.profile.exception.UserNotFoundException;
+import org.deltacore.delta.shared.exception.AdviceStruct;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authorization.AuthorizationDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.Instant;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 @RestControllerAdvice
-public class AuthExceptionAdvice {
+public class AuthExceptionAdvice extends AdviceStruct {
     @ExceptionHandler(InvalidTokenException.class)
     public ResponseEntity<?> handleBusiness(InvalidTokenException ex) {
         return ResponseEntity
@@ -33,5 +39,10 @@ public class AuthExceptionAdvice {
         return ResponseEntity
                 .status(HttpStatus.CONFLICT)
                 .body(Map.of("Error:", "User already exists: " + ex.getMessage()));
+    }
+
+    @ExceptionHandler(InvalidRecoveryCodeException.class)
+    public ResponseEntity<Map<String, Object>> handleProfileImageNotSetException(InvalidRecoveryCodeException ex, HttpServletRequest request) {
+        return getMapResponseEntity(request, ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 }
