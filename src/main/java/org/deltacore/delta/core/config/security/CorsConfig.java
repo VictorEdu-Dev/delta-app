5dab34aaa22ce584880acc7de95d8335b7f0bd83
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-25 11:16:21 -0300

    feat: add CORS configuration to allow specific origins and methods

---
+++ src/main/java/org/deltacore/delta/core/config/security/CorsConfig.java
@@ -0,0 +1,30 @@
+package org.deltacore.delta.core.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "https://delta-front-app.vercel.app",
+                "https://delta-app-410394653851.southamerica-east1.run.app",
+                "https://delta-app-ekp2l6euwq-rj.a.run.app"
+        ));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept", "Origin", "Cache-Control", "X-Requested-With"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
