bb8513cf76820101d86aeaaa802e91878099cef1
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-25 15:05:57 -0300

    feat: add FileTooLargeException for handling file size errors

---
+++ src/main/java/org/deltacore/delta/domains/profile/exception/FileTooLargeException.java
@@ -0,0 +1,7 @@
+package org.deltacore.delta.domains.profile.exception;
+
+public class FileTooLargeException extends RuntimeException {
+    public FileTooLargeException(String message) {
+        super(message);
+    }
+}
