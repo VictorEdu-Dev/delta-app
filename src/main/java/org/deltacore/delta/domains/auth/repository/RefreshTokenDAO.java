555a9a2c8b4b39d666418a88721863f019b538b0
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-29 00:22:31 -0300

    feat: add methods to manage revoked refresh tokens in RefreshTokenDAO

--- src/main/java/org/deltacore/delta/domains/auth/repository/RefreshTokenDAO.java
+++ src/main/java/org/deltacore/delta/domains/auth/repository/RefreshTokenDAO.java
@@ -1,6 +1,9 @@
 package org.deltacore.delta.domains.auth.repository;
 
+import jakarta.transaction.Transactional;
 import org.deltacore.delta.domains.auth.model.RefreshToken;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -17,4 +20,16 @@ public interface RefreshTokenDAO extends CrudRepository<RefreshToken, Long> {
 
     @Query("SELECT rt FROM RefreshToken rt JOIN FETCH rt.user u WHERE rt.token = :refreshToken")
     Optional<RefreshToken> findByRefreshToken(@Param("refreshToken") UUID refreshToken);
+
+
+    @Query("SELECT rt.id FROM RefreshToken rt WHERE rt.revoked = true")
+    List<Long> findRevokedTokenIds(Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE RefreshToken t SET t.revoked = true WHERE t.expiresAt < CURRENT_TIMESTAMP AND t.revoked = false")
+    void markExpiredTokensAsRevoked();
+
+    @Transactional
+    void deleteByIdIn(List<Long> ids);
 }
