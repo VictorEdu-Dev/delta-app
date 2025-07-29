c21aeb10205253bdc752def03fd0815ec2c3a00c
Victor Eduardo <victoreduardodev@gmail.com>
2025-07-29 00:22:36 -0300

    feat: refactor SharedExceptionHandler to streamline exception handling and improve response structure

--- src/main/java/org/deltacore/delta/shared/rest/SharedExceptionHandler.java
+++ src/main/java/org/deltacore/delta/shared/rest/SharedExceptionHandler.java
@@ -1,128 +1,69 @@
 package org.deltacore.delta.shared.rest;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.deltacore.delta.shared.exception.ConflictException;
-import org.deltacore.delta.domains.activity.exception.LargeFileException;
-import org.deltacore.delta.shared.exception.ResourceNotFoundException;
+import org.deltacore.delta.shared.exception.*;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authorization.AuthorizationDeniedException;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.nio.file.NoSuchFileException;
-import java.time.Instant;
-import java.util.HashMap;
 import java.util.Map;
 
 @RestControllerAdvice
-public class SharedExceptionHandler {
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        StringBuilder messages = new StringBuilder();
-        BindingResult bindingResult = e.getBindingResult();
-
-        bindingResult.getFieldErrors().forEach(error -> {
-            messages.append(error.getDefaultMessage()).append(" \n");
-        });
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(messages.toString());
-    }
-
+public final class SharedExceptionHandler extends AdviceStruct {
     @ExceptionHandler(ConflictException.class)
-    public ResponseEntity<?> handleBusiness(ConflictException ex) {
-        return ResponseEntity
-                .status(HttpStatus.CONFLICT)
-                .body(Map.of("Error:", ex.getMessage()));
+    public ResponseEntity<?> handleBusiness(ConflictException ex, HttpServletRequest request) {
+        return getMapResponseEntity(request, GeneralErrorCode.CONFLICT.name(), ex.getMessage(), HttpStatus.CONFLICT);
     }
 
-    @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<Map<String, Object>> handleNotFound(ResourceNotFoundException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("Error:", ex.getMessage());
-        body.put("Status:", HttpStatus.NOT_FOUND.value());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    @ExceptionHandler(EmptyFileException.class)
+    public ResponseEntity<?> handleEmptyFileException(EmptyFileException ex, HttpServletRequest request) {
+        return getMapResponseEntity(request, GeneralErrorCode.EMPTY_FILE.name(),
+                ex.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(UsernameNotFoundException.class)
-    public ResponseEntity<?> handleUsernameNotFound(UsernameNotFoundException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("Error:", ex.getMessage());
-        body.put("Status:", HttpStatus.NOT_FOUND.value());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    @ExceptionHandler(FileTooLargeException.class)
+    public ResponseEntity<?> handleFileTooLargeException(FileTooLargeException ex, HttpServletRequest request) {
+        return getMapResponseEntity(request, GeneralErrorCode.FILE_TOO_LARGE.name(),
+                ex.getMessage(), HttpStatus.PAYLOAD_TOO_LARGE);
     }
 
-    @ExceptionHandler(BadCredentialsException.class)
-    public ResponseEntity<?> handleBadCredentials(BadCredentialsException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("Error:", ex.getMessage());
-        body.put("Status:", HttpStatus.UNAUTHORIZED.value());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(body);
-    }
-
-    @ExceptionHandler(AuthorizationDeniedException.class)
-    public ResponseEntity<?> handleAuthorizationDenied(AuthorizationDeniedException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("Error:", ex.getMessage());
-        body.put("Status:", HttpStatus.FORBIDDEN.value());
-        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(body);
+    @ExceptionHandler(InvalidFileTypeException.class)
+    public ResponseEntity<?> handleInvalidFileTypeException(InvalidFileTypeException ex, HttpServletRequest request) {
+        return getMapResponseEntity(request, GeneralErrorCode.INVALID_FILE_TYPE.name(),
+                ex.getMessage(), HttpStatus.UNSUPPORTED_MEDIA_TYPE);
     }
 
     @ExceptionHandler(NoSuchFileException.class)
-    public ResponseEntity<?> handleNoSuchFileException(NoSuchFileException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("Error:", ex.getMessage());
-        body.put("Status:", HttpStatus.NOT_FOUND.value());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    public ResponseEntity<?> handleNoSuchFileException(NoSuchFileException ex, HttpServletRequest request) {
+        return getMapResponseEntity(request, GeneralErrorCode.NO_SUCH_FILE.name(),
+                ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(NoResourceFoundException.class)
-    public ResponseEntity<?> handleNoResourceFoundException(NoResourceFoundException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("Error:", ex.getMessage());
-        body.put("Status:", HttpStatus.NOT_FOUND.value());
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    public ResponseEntity<?> handleNoResourceFoundException(NoResourceFoundException ex, HttpServletRequest request) {
+        return getMapResponseEntity(request, GeneralErrorCode.RESOURCE_NOT_FOUND.name(),
+                ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(LargeFileException.class)
-    public ResponseEntity<?> handleLargeFileException(LargeFileException ex) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("Error:", ex.getMessage());
-        body.put("Status:", HttpStatus.PAYLOAD_TOO_LARGE.value());
-        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(body);
-    }
-
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<Map<String, Object>> handleAllExceptions(Exception ex, HttpServletRequest request) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", Instant.now().toString());
-        body.put("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
-        body.put("error", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
-        body.put("message", "Ocorreu um erro interno inesperado.");
-        body.put("details", ex.getMessage());
-        body.put("path", request.getRequestURI());
-
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(body);
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<?> handleResourceNotFoundException(ResourceNotFoundException ex, HttpServletRequest request) {
+        return getMapResponseEntity(request, GeneralErrorCode.RESOURCE_NOT_FOUND.name(),
+                ex.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MaxUploadSizeExceededException.class)
     public ResponseEntity<Map<String, Object>> handleMaxUploadSizeExceededException(MaxUploadSizeExceededException ex, HttpServletRequest request) {
-        Map<String, Object> body = new HashMap<>();
-        body.put("timestamp", Instant.now().toString());
-        body.put("status", HttpStatus.PAYLOAD_TOO_LARGE.value());
-        body.put("error", HttpStatus.PAYLOAD_TOO_LARGE.getReasonPhrase());
-        body.put("message", "O tamanho do arquivo enviado excede o limite permitido.");
-        body.put("details", ex.getMessage());
-        body.put("path", request.getRequestURI());
+        return getMapResponseEntity(request, GeneralErrorCode.FILE_TOO_LARGE.name(),
+                ex.getMessage(), HttpStatus.PAYLOAD_TOO_LARGE);
+    }
 
-        return ResponseEntity.status(HttpStatus.PAYLOAD_TOO_LARGE).body(body);
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, Object>> handleAllExceptions(Exception ex, HttpServletRequest request) {
+        return getMapResponseEntity(request, GeneralErrorCode.INTERNAL_ERROR.name(),
+                ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
